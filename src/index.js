e28faf04463b74d8a762feadf708c8167a8ca8bf
rasul-surname <rasul.surname@mail.ru>
2022-07-06 18:06:54 +0300

    передал список постов через recoil

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import {ColorModeScript} from '@chakra-ui/react';
+import React, {StrictMode} from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import {RecoilRoot} from "recoil";
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <ColorModeScript/>
+        <RecoilRoot>
+            <App/>
+        </RecoilRoot>
+    </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
